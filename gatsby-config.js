6f04bc58d848d3abcf2dcf2f2bb46ce17d3122b4
Wesley Tan <wesleysimeontan@gmail.com>
2024-01-10 13:18:07 +0800

    New commits

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Wesley Tan's Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
