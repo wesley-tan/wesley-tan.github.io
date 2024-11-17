cf3ccf3799fee6ed5acd97adaa4faed8c569efb8
Wesley Tan <wesleysimeontan@gmail.com>
2024-11-16 23:05:49 -0500

    New files

--- .cache/async-requires.js
+++
@@ -1,7 +0,0 @@
-exports.components = {
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-projects-js": () => import("./../../../src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */),
-  "component---src-pages-tutoring-js": () => import("./../../../src/pages/tutoring.js" /* webpackChunkName: "component---src-pages-tutoring-js" */)
-}
-
